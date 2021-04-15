bc09769e096833ee3a094680f2f8b0a5534189d4
unknown <Alex Quix>
2021-04-15 11:56:32 -0600

    add isomorphic effect in the card-media component

--- src/components/card-media.tsx
+++ src/components/card-media.tsx
@@ -13,7 +13,26 @@ function Card({result, mediatype}:Props){
     if(mediatype == "movie"){
         let data = result as Movie.Search;
         return (
-            <div className={style["container"]}>
+            <a className={style["container"]}>
+                <div className={style["wrapper"]}>
+                    <div className={style["wrapper-ratingstar"]}>
+                        <RatingStar rating={data.vote_average}/>
+                    </div>
+                    <div className={style["wrapper-img"]}>
+                        <img src={process.env.NEXT_PUBLIC_URL_IMG + data.poster_path} alt=""/>
+                    </div>
+                    <div className={style["wrapper-info"]}>
+                        <h1 className={style["title"]}>{data.title}</h1>
+                        <h3 className={style["date"]}>{data.release_date}</h3>
+                    </div>
+                </div>
+            </a>
+        )
+    }
+    let data = result as TVShow.Search;
+    return (
+        <a className={style["container"]}>
+            <div className={style["wrapper"]}>
                 <div className={style["wrapper-ratingstar"]}>
                     <RatingStar rating={data.vote_average}/>
                 </div>
@@ -21,26 +40,11 @@ function Card({result, mediatype}:Props){
                     <img src={process.env.NEXT_PUBLIC_URL_IMG + data.poster_path} alt=""/>
                 </div>
                 <div className={style["wrapper-info"]}>
-                    <h1 className={style["title"]}>{data.title}</h1>
-                    <h3 className={style["date"]}>{data.release_date}</h3>
+                    <h1 className={style["title"]}>{data.name}</h1>
+                    <h3 className={style["date"]}>{data.first_air_date}</h3>
                 </div>
             </div>
-        )
-    }
-    let data = result as TVShow.Search;
-    return (
-        <div className={style["container"]}>
-            <div className={style["wrapper-ratingstar"]}>
-                <RatingStar rating={data.vote_average}/>
-            </div>
-            <div className={style["wrapper-img"]}>
-                <img src={process.env.NEXT_PUBLIC_URL_IMG + data.poster_path} alt=""/>
-            </div>
-            <div className={style["wrapper-info"]}>
-                <h1 className={style["title"]}>{data.name}</h1>
-                <h3 className={style["date"]}>{data.first_air_date}</h3>
-            </div>
-        </div>
+        </a>
     )
 };
 
